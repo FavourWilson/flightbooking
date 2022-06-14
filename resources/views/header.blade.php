864affc8b98ffb7b06741b92496a09c2afe38e41
Ebrusike Favour <ebrusikefavour@gmail.com>
2022-06-14 07:23:36 -0700

    changes to css file path

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -4,14 +4,14 @@
 <meta charset="utf-8">
 <title>Treker</title>
 <!-- Stylesheets -->
-<link href="{{asset('/css/bootstrap.css')}}" rel="stylesheet">
-<link href="{{asset('/css/style.css')}}" rel="stylesheet">
+<link href="{{asset('public/css/bootstrap.css')}}" rel="stylesheet">
+<link href="{{asset('public/css/style.css')}}" rel="stylesheet">
 <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
-<link rel="icon" href="{{asset('images/favicon.png')}}" type="image/x-icon">
+<link rel="icon" href="{{asset('public/images/favicon.png')}}" type="image/x-icon">
 <!-- Responsive -->
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-<link href="{{asset('/css/responsive.css')}}" rel="stylesheet">
+<link href="{{asset('public/css/responsive.css')}}" rel="stylesheet">
 <!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script><![endif]--> 
 
 </head>
